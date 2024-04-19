d505e0fe3d1c9ceb5bb95b3a31045225f2db5de6
DAVID SANTIAGO PIEDRAHITA CABEZA <145588434+santkd16@users.noreply.github.com>
2024-04-19 15:29:31 -0500

    Update PuntoTiempo.java

--- PuntoTiempo.java
+++ PuntoTiempo.java
@@ -1,25 +1,23 @@
-public class PuntoTiempo extends Punto {
+
+public class PuntoTiempo extends Punto { 
+
     private int t;
-
-    public PuntoTiempo() {
-        super();
-        this.setT(0);
+    
+    public PuntoTiempo () 
+    {this (0,0,0);}
+    
+    
+    
+    public PuntoTiempo (int x, int y, int t) 
+    
+    {super (x, y);
+    this.setT (t);
     }
-
-    public PuntoTiempo(int x, int y, int t) {
-        super(x, y);
-        this.setT(t);
-    }
-
-    public int getT() {
-        return this.t;
-    }
-
-    public final void setT(int t) {
-        this.t = t;
-    }
-
-    public double velocidad() {
-        return this.modulo() / this.getT();
-    }
+    
+    public int getT() {return this.t;}
+    
+    public final void setT (int t) {this.t = t;}
+    
+    public double velocidad () 
+    {return this.modulo ()/this.getT();}
 }
