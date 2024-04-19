94cd238f1c58dc2e411a2001ff1d2948a6e46fe8
DAVID SANTIAGO PIEDRAHITA CABEZA <145588434+santkd16@users.noreply.github.com>
2024-04-19 15:29:07 -0500

    Update Punto.java

--- Punto.java
+++ Punto.java
@@ -1,32 +1,28 @@
-public class Punto {
+public class Punto { 
     private int x, y;
-
-    public Punto(int x, int y) {
-        this.setX(x);
-        this.setY(y);
+    
+    public Punto (int x, int y) { 
+    this.x=x; 
+    this.y=y;
+    
     }
-
-    public Punto() {
-        this(0, 0);
-    }
-
-    public final void setX(int x) {
-        this.x = x;
-    }
-
-    public final void setY(int y) {
-        this.y = y;
-    }
-
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY() {
-        return this.y;
-    }
-
-    public double modulo() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+    
+    public Punto (){
+        this.x=0;  
+        this.y=0;
+ }
+     
+    
+    
+    public final void setX (int x) {this.x = x; }
+    
+    public final void sety (int y) {this.y = y; }
+    
+    public int getX () {return this.x;} 
+    public int getY() {return this.y;}
+    
+    public double modulo () 
+    {
+    return Math.sqrt((this.x * this.x) +(this.y * this.y));
     }
 }
