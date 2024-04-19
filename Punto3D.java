0efc4bbd174131192be4e8574983f909da1566e0
DAVID SANTIAGO PIEDRAHITA CABEZA <145588434+santkd16@users.noreply.github.com>
2024-04-19 15:35:04 -0500

    Update Punto3D.java

--- Punto3D.java
+++ Punto3D.java
@@ -1,22 +1,19 @@
-
-public class Punto3D extends PuntoTiempo {
+public class Punto3D extends PuntoTiempo { 
     private int z;
-
-    public Punto3D(int x, int y, int z, int t) {
-        super(x, y, t);
-        this.setZ(z);
-    }
-
-    public int getZ() {
-        return this.z;
-    }
-
-    public final void setZ(int z) {
-        this.z = z;
-    }
-
+    
+    public Punto3D (int x, int y, int z, int t) 
+    
+    { super (x, y, t);
+    this.setz (z);}
+    
+    
+    public int getz () {return this.z;}
+    
+    public final void setz (int z) { this.zz;}
+    
     @Override
-    public double modulo() {
-        return Math.sqrt(this.getX() * this.getX() + this.getY() * this.getY() + this.z * this.z);
+    public double modulo () {
+    return Math.sqrt(this.getX () * this.getX() +this.getY()* this.getY()+this.z*this.z);
     }
+
 }
